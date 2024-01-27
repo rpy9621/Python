b231177b0b5fe54f5721981e6dfd2698ace9f904
-Ramendra <121676584+rpy9621@users.noreply.github.com>
2024-01-27 20:40:40 +0530

    Add files via upload

---
+++ taking_input.py
@@ -0,0 +1,13 @@
+#taking input in python
+a=input("Enter your name : ")
+print("Your name is :",a)
+b=int(input("Enter first num : "))
+c=float(input("Enter second num : "))
+sum=b+c
+print("Sum of two number is : ",sum)
+print("\n")
+print(type(sum))
+print("\n")
+print(type(a))
+f=list(input("Enter elements in list"))
+print("Element's of list is :",f)
